fd105c8473dc5296398b0b4c18235ae5d7b44f78
TausK <36681180+TausK@users.noreply.github.com>
2018-10-30 12:01:12 +1100

    GUI

---
+++ Assets/Scripts/Enemy/Random_Nav.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Random_Nav : MonoBehaviour
+{
+    private NavMeshAgent nav;
+    private Vector3 startPosition;
+    public float roamRadius;
+
+    void Awake()
+    {
+        startPosition = transform.position;
+    }
+
+    void FreeRoam()
+    {
+        {
+            Vector3 randomDir = Random.insideUnitSphere * roamRadius;
+            randomDir += startPosition;
+            NavMeshHit hit;
+            NavMesh.SamplePosition(randomDir, out hit, roamRadius, 1);
+            Vector3 finalPosition = hit.position;
+            nav.destination = finalPosition;
+        }
+    }
+
+
+}
--- Assets/Scripts/GUI/MainMenu.cs
+++ Assets/Scripts/GUI/MainMenu.cs
@@ -6,14 +6,58 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    
-    public void PlayBtn(string loadLvl)
+    public GameObject StartMenu;
+    public GameObject LoginMenu;
+    public GameObject CharacterSelection;
+    public GameObject CreateNewChar;
+
+    private void Start()
     {
-        SceneManager.LoadScene(loadLvl);
+        //First panel is active at start
+        StartMenu.SetActive(true);
+    }
+    public void Play()
+    {
+        //Once button pressed then
+        //StartMenu is false
+        StartMenu.SetActive(false);
+        //LoginMenu is true
+        LoginMenu.SetActive(true);
+    }
+
+
+    public void Login()
+    {
+        //If successful then...
+        //Allow login   
+        //otherwise
+        //Create new character
+
+        //Move to character custom
+        LoginMenu.SetActive(false);
+        CharacterSelection.SetActive(true);
+    }
+
+    public void CharSelect()
+    {
+        //Once Character is selected then...
+            //Load Game
+    }
+
+    public void Back()
+    {
+        //if the button is pressed then....
+            //Toggle back to previous panel
+
     }
 
     public void ExitBtn()
     {
+        //Close application within editor mode
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        //Close Application
         Application.Quit();
     }
 }
