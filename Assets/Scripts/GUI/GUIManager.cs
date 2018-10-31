080da33658588f26ecb51caa23d5034dee29fa10
TausK <36681180+TausK@users.noreply.github.com>
2018-10-31 11:47:20 +1100

    GUI

---
+++ Assets/Scripts/GUI/GUIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GUIManager : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject Options;
+    public GameObject Login;
+    public GameObject CreateUser;
+    public GameObject CharSelect;
+    public GameObject CreateChar;
+
+  
+    
+    public void Back()
+    {
+        //if the button is pressed then....
+            //Toggle back to previous panel
+
+    }
+
+    public void ExitBtn()
+    {
+        //Close application within editor mode
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        //Close Application
+        Application.Quit();
+    }
+}
