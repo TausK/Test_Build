4f96f4a364d20bf889a87af89bbd7f9ec31362a8
TausK <36681180+TausK@users.noreply.github.com>
2018-11-07 11:58:18 +1100

    GUI Setup

--- Assets/Scripts/GUI/GUIManager.cs
+++ Assets/Scripts/GUI/GUIManager.cs
@@ -6,20 +6,48 @@ using UnityEngine.SceneManagement;
 
 public class GUIManager : MonoBehaviour
 {
+    #region Menu 
+    //Beginning of menu
+    public GameObject StartMenu;
+    //Main Menu
     public GameObject MainMenu;
+    //Options Menu
     public GameObject Options;
+    //Login Menu
     public GameObject Login;
+    //Create user menu
     public GameObject CreateUser;
+    //Character Selection Menu
     public GameObject CharSelect;
+    //Character Creating Menu
     public GameObject CreateChar;
+    #endregion
 
-  
-    
+
+    private void Start()
+    {
+        StartMenu.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            StartMenu.SetActive(false);
+            MainMenu.SetActive(true);
+        }  
+    }
+
+    public void PlayBtn()
+    {
+
+    }
+
+
     public void Back()
     {
         //if the button is pressed then....
             //Toggle back to previous panel
-
     }
 
     public void ExitBtn()
