7a94fc44276a7cdf2a2489ba6e1e789b3acbdf00
TausK <36681180+TausK@users.noreply.github.com>
2018-11-07 15:52:09 +1100

    GUI

--- Assets/Scripts/GUI/GUIManager.cs
+++ Assets/Scripts/GUI/GUIManager.cs
@@ -8,46 +8,85 @@ public class GUIManager : MonoBehaviour
 {
     #region Menu 
     //Beginning of menu
-    public GameObject StartMenu;
+    public GameObject startMenu;
+    //GUI Pop up in main menu
+    public GameObject startMenuGUI;
     //Main Menu
-    public GameObject MainMenu;
+    public GameObject mainMenu;
     //Options Menu
-    public GameObject Options;
+    public GameObject options;
     //Login Menu
-    public GameObject Login;
+    public GameObject login;
     //Create user menu
-    public GameObject CreateUser;
+    public GameObject createUser;
     //Character Selection Menu
-    public GameObject CharSelect;
+    public GameObject charSelect;
     //Character Creating Menu
-    public GameObject CreateChar;
+    public GameObject createChar;
+    #endregion
+
+    #region
+    public bool isMain;
+    public bool iscCurMenu;
+    
+    //public int index;
+
+    //public GameObject[] menus;
+
     #endregion
 
 
     private void Start()
     {
-        StartMenu.SetActive(true);
+        startMenu.SetActive(true);
     }
 
     private void Update()
     {
         if (Input.anyKeyDown)
         {
-            StartMenu.SetActive(false);
-            MainMenu.SetActive(true);
-        }  
+            startMenu.SetActive(false);
+            startMenuGUI.SetActive(true);
+            return;
+        }
+
+        if(!mainMenu.activeSelf)
+        {
+            isMain = false;
+           
+        }
+
+        if (mainMenu.activeSelf)
+        {
+            isMain = true;
+        }
     }
 
     public void PlayBtn()
     {
-
+        mainMenu.SetActive(false);
+         login.SetActive(true);
     }
+
+    public void OptionBtn()
+    {
+        mainMenu.SetActive(false);
+        options.SetActive(true);
+    }
+
 
 
     public void Back()
     {
+        login.SetActive(false);
+        options.SetActive(false);
+        createUser.SetActive(false);
+        createChar.SetActive(false);
+
+
         //if the button is pressed then....
-            //Toggle back to previous panel
+        //Toggle back to previous panel
+        mainMenu.SetActive(true);
     }
 
     public void ExitBtn()
