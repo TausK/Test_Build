e3e62cff18b537ae22c01f70c713b4e5aed88937
TausK <36681180+TausK@users.noreply.github.com>
2018-11-11 22:17:31 +1000

    GUI

---
+++ Assets/NetworkTests/Scripts/CustomNetwork.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class CustomNetwork : NetworkManager
+{
+
+    public void StartUpHost()
+    {
+
+    }
+
+    public void JoinGame()
+    {
+
+    }
+}
