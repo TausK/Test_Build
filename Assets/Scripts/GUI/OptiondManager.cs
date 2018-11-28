f7d4fadaa902b164b92446d31d85f25edd32b59c
TausK <36681180+TausK@users.noreply.github.com>
2018-11-28 18:52:45 +1000

    AI

---
+++ Assets/Scripts/GUI/OptiondManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class OptiondManager : MonoBehaviour
+{
+    #region Variables
+    [Header("Resolutuion")]
+    //Vector2 array for resolution
+    public Vector2[] res = new Vector2[4];
+    public int resIndex;
+    //resolution dropdown ui
+    public Dropdown resDrop;
+
+    //public AudioSource audio;
+    //public Light light;
+    //public Slider volSlide, brightSlide;
+    #endregion
+    // Use this for initialization
+    void Start()
+    {
+        //find gameobject with name/tag "Music" and component 
+        //audio = GameObject.Find("Music").GetComponent<AudioSource>();
+        //light = GameObject.Find("Directional Light").GetComponent<Light>();
+    }
+
+    public void Resolutions()
+    {
+        //index equal to dropdown values
+        resIndex = resDrop.value;
+        //Set resolution value functionality
+        Screen.SetResolution((int)res[resIndex].x, (int)res[resIndex].y,FullScreenMode.FullScreenWindow);
+    }
+
+
+}
---
+++ Assets/Scripts/Menus/MenuHandler.cs
@@ -0,0 +1,79 @@
+﻿//using System.Collections;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;//interacting with scene change
+//using UnityEngine.UI;//interacting with GUI elements
+//using UnityEngine.EventSystems;//control the event (button shiz)
+//[AddComponentMenu("Skyrim2.0/Menus/Main")]
+//public class OptionsManager : MonoBehaviour
+//{
+//    #region Variables
+//    [Header("OPTIONS")]
+//    public Vector2[] res = new Vector2[7];
+//    public int resIndex;
+//    public bool isFullScreen;
+//    [Header("References")]
+//    public AudioSource mainAudio;
+//    public Light dirLight;
+//    public Dropdown resDropdown;
+//    public Slider volSlider, brightSlider;
+
+//    #endregion
+//    void Start()
+//    {
+//        mainAudio = GameObject.Find("MainMusic").GetComponent<AudioSource>();
+//        dirLight = GameObject.Find("Directional Light").GetComponent<Light>();
+
+//    }
+//    public void LoadGame()
+//    {
+//        SceneManager.LoadScene(1);
+//    }
+//    public void ExitGame()
+//    {
+//#if UNITY_EDITOR
+//        UnityEditor.EditorApplication.isPlaying = false;
+//#endif
+//        Application.Quit();
+//    }
+
+//    {
+
+//            volSlider = GameObject.Find("AudioSlider").GetComponent<Slider>();
+
+//            brightSlider = GameObject.Find("BrightSlider").GetComponent<Slider>();
+
+//            resDropdown = GameObject.Find("Resolution").GetComponent<Dropdown>();
+
+//            volSlider.value = mainAudio.volume;
+//            brightSlider.value = dirLight.intensity;
+
+//            return true;
+//        }
+//    }
+//    public void Volume()
+//{
+//    mainAudio.volume = volSlider.value;
+//}
+//public void Brightness()
+//{
+//    dirLight.intensity = brightSlider.value;
+//}
+
+//public void Resolution()
+//{
+//    resIndex = resDropdown.value;
+//    Screen.SetResolution((int)res[resIndex].x, (int)res[resIndex].y, isFullScreen);
+//}
+//    //public void Save()
+//    //{
+//    //    PlayerPrefs.SetString("Forward", forward.ToString());
+//    //    PlayerPrefs.SetString("Backward", backward.ToString());
+//    //    PlayerPrefs.SetString("Left", left.ToString());
+//    //    PlayerPrefs.SetString("Right", right.ToString());
+//    //    PlayerPrefs.SetString("Jump", jump.ToString());
+//    //    PlayerPrefs.SetString("Crouch", crouch.ToString());
+//    //    PlayerPrefs.SetString("Sprint", sprint.ToString());
+//    //    PlayerPrefs.SetString("Interact", interact.ToString());
+//    //}
+
+//}
