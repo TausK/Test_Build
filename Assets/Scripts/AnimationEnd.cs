09ab8956205e4e6a43bc384ff9d318643fce10b8
TausK <36681180+TausK@users.noreply.github.com>
2018-12-07 20:59:28 +1000

    Cinematic

---
+++ Assets/Scripts/AnimationEnd.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimationEnd : MonoBehaviour
+{
+    public float timer = 5;
+    public float scenTimer;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        scenTimer += Time.deltaTime;
+        if(scenTimer >= timer)
+        {
+            scenTimer = 0;
+            Debug.Log("times up");
+            SceneManager.LoadScene(2);
+        }
+    }
+}
